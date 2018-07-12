8c895573b5d93fb1924a1303f7a4e4813db1bffb
Juan Ignacio Cuiule <juanchicuiu@gmail.com>
2018-07-12 02:46:19 -0300

    argego carpeta modelos y modelo usuario

---
+++ Aplicacion Desktop/FrbaHotel/Modelos/Usuario.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FrbaHotel.Modelos
+{
+    public class Usuario
+    {
+        public int id_usuario;
+        public String username;
+
+        public Usuario(int id_usuario, String username)
+        {
+            this.id_usuario = id_usuario;
+            this.username = username;
+        }
+    }
+}
